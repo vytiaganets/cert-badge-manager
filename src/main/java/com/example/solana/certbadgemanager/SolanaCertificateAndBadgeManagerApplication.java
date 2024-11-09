d641964ce0e39a08ac8bd565862d6a05e12def44
Andrii <vytiaganets@ukr.net>
2024-11-09 12:18:49 +0100

    create

---
+++ src/main/java/com/example/solana/certbadgemanager/SolanaCertificateAndBadgeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.solana.certbadgemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolanaCertificateAndBadgeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolanaCertificateAndBadgeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/solana/certbadgemanager/SolanaCertificateAndBadgeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solana.certbadgemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolanaCertificateAndBadgeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
